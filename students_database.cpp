65bb41c4daa49ddaba6537473632230f675b9a86
Vlad Ishchuk <vlad.ischuck2006@gmail.com>
2023-09-26 18:08:27 +0300

    Update

--- students_database.cpp
+++ students_database.cpp
@@ -11,9 +11,6 @@
 
 using namespace std;
 
-const string database_file = "./database.txt";
-
-
 void info_message (string text) {
     std::cout << "\033[1;33m" << text << "\033[0m" << std::endl;
 }
@@ -22,58 +19,62 @@ void successful_message (string text) {
     std::cout << "\033[1;32m" << text << "\033[0m" << std::endl;
 }
 
-void save_data (string data) {
-    std::ofstream database(database_file, std::ios_base::app);
-    
-    if (!database) {
-        std::cout << "Database is not created!";
-        return;
+class Database {
+private:
+    string database_file;
+public:
+    Database(string db_file) {
+        database_file = db_file;
     }
     
-    database << data << '\n';
-    
-    database.close();
-}
+    void save_data (string data) {
+        std::ofstream database(database_file, std::ios_base::app);
+        
+        if (!database) {
+            throw std::runtime_error("Database is not created!");
+        }
+        
+        database << data << '\n';
+        
+        database.close();
+    }
 
-void read_data (vector<string> &data) {
-    std::fstream database;
-    database.open(database_file, ios::in);
-    
-    if (!database) {
-        info_message("No such database file!");
-        return;
+    void read_data (vector<string> &data) {
+        std::fstream database;
+        database.open(database_file, ios::in);
+        
+        if (!database) {
+            throw std::runtime_error("No such database file!");
+        }
+        
+        std::string line;
+        while (std::getline(database, line)) {
+            data.push_back(line);
+        }
+        
+        database.close();
     }
-    
-    std::string line;
-    while (std::getline(database, line)) {
-        data.push_back(line);
-    }
-    
-    database.close();
-}
 
-void update_data (vector<string> &data) {
-    std::ofstream database(database_file, ios::out);
+    void update_data (vector<string> &data) {
+        std::ofstream database(database_file, ios::out);
 
-    if (!database) {
-        std::cout << "Database is not found!";
-        return;
+        if (!database) {
+            throw std::runtime_error("Database is not found!");
+        }
+        
+        for (int i = 0; i < data.size(); i++) {
+            database << data[i] << '\n';
+        }
+        
+        database.close();
     }
-    
-    for (int i = 0; i < data.size(); i++) {
-        database << data[i] << '\n';
-    }
-    
-    database.close();
-}
+};
 
-// Функція для відображення більш-менш відформатованої таблиці
+// ============ Форматування таблиць ============
 void print_table (vector<string> &data) {
     if (data.size() == 0) {
-        // Виведемо повідомлення, що свідчить про відсутність записів у БД
         info_message("The list of records is empty.");
     } else {
-        // Виводимо таблицю сірого кольору
         std::cout << "\n\033[1;37m========== Records list ==========" << std::endl;
         for (int i = 0; i < data.size(); i++) {
             cout << "| " << i << ". " << data[i] << endl;
@@ -82,64 +83,119 @@ void print_table (vector<string> &data) {
     }
 }
 
+void print_table_with_cursor (vector<string> &data, int pointer, string pointer_message) {
+    if (data.size() == 0) {
+        info_message("The list of records is empty.");
+    } else {
+        std::cout << "\n" << "\033[1;37m" << "========== Records list ==========" << std::endl;
+        for (int i = 0; i < data.size(); i++) {
+            if (pointer == i) {
+                cout << "| " << i << ". " << data[i] << "\033[3;90m <- " << pointer_message << "\033[0m\033[1;37m" << endl;
+            } else {
+                cout << "| " << i << ". " << data[i] << endl;
+            }
+        }
+        std::cout << "==================================\033[0m" << std::endl << std::endl;
+    }
+}
+// ==============================================
+
+
 int main () {
+    Database db("./database.txt");
+    
     while (true) {
-        char operation;
-        
-        std::cout << "\n\033[1;37mChoose operation (0 - list of records, 1 - make new record, 2 - delete a record, 'q' - exit): \033[0m";
-        std::cin >> operation;
-        
-        switch (operation) {
-            case '0': {
-                vector<string> data;
-                read_data(data);
-                print_table(data);
-                break;
-            }
-                
-            case '1': {
-                string entered_data;
-                std::cout << "> ";
-                // Заміна cin
-                std::getline(std::cin >> ws, entered_data);
-                
-                save_data(entered_data);
-                successful_message("The record was created successfully!");
-                
-                break;
-            }
-                
-            case '2': {
-                vector<string> data;
-                read_data(data);
-                
-                int delete_index;
-                
-                std::cout << "Enter index of record you want to delete: ";
-                std::cin >> delete_index;
-                
-                // Перевіряємо, чи даний індекс існує
-                if (delete_index >= 0 && delete_index < data.size()) {
-                    // Видаляємо елемент за вказаним індексом
-                    data.erase(data.begin() + delete_index);
-                    successful_message("Element at index " + to_string(delete_index) + " has been deleted.");
-                } else {
-                    info_message("Invalid index.");
+        try {
+            char operation;
+            
+            std::cout << "\n\033[1;37mChoose operation ('c' - create a new record, 'r' - list of records, 'u' - update a record, 'd' - delete a record, 'q' - exit): \033[0m";
+            std::cin >> operation;
+            
+            switch (operation) {
+                case 'r': {
+                    vector<string> data;
+                    
+                    db.read_data(data);
+                    print_table(data);
+                    
+                    break;
                 }
-                
-                print_table(data); // Виводимо нову таблицю записів
-                update_data(data); // Оноволюємо дані БД
-                
-                break;
+                    
+                case 'c': {
+                    string entered_data;
+                    std::cout << "> ";
+                    std::getline(std::cin >> ws, entered_data);
+                    
+                    db.save_data(entered_data);
+                    successful_message("The record was created successfully!");
+                    
+                    break;
+                }
+                    
+                case 'd': {
+                    vector<string> data;
+                    int delete_index;
+                    
+                    db.read_data(data);
+                    
+                    std::cout << "Enter index of record you want to delete: ";
+                    std::cin >> delete_index;
+                    
+                    // Перевіряємо, чи даний індекс існує
+                    if (delete_index >= 0 && delete_index < data.size()) {
+                        // Видаляємо елемент за вказаним індексом
+                        data.erase(data.begin() + delete_index);
+                        successful_message("Element at index " + to_string(delete_index) + " has been deleted.");
+                    } else {
+                        info_message("Invalid index.");
+                    }
+                    
+                    db.update_data(data); // Оноволюємо дані БД
+                    print_table(data); // Виводимо нову таблицю записів
+                    
+                    break;
+                }
+                    
+                case 'u': {
+                    vector<string> data;
+                    int update_index;
+                    
+                    db.read_data(data);
+                    
+                    std::cout << "Enter index of record you want to update: ";
+                    std::cin >> update_index;
+                    
+                    // Перевіряємо, чи даний індекс існує
+                    if (update_index >= 0 && update_index < data.size()) {
+                        string entered_data;
+                        std::cout << "> ";
+                        std::getline(std::cin >> ws, entered_data);
+                        
+                        string old_data = data[update_index];
+                        
+                        data[update_index] = entered_data;
+                        
+                        successful_message("Element at index " + to_string(update_index) + " has been updated from '" + old_data + "' to '" + entered_data + "'.");
+                    } else {
+                        info_message("Invalid index.");
+                    }
+                    
+                    db.update_data(data); // Оноволюємо дані БД
+                    print_table_with_cursor(data, update_index, "Updated"); // Виводимо нову таблицю записів
+                    
+                    break;
+                }
+                    
+                case 'q':
+                    return 0;
+                    break;
+                    
+                default:
+                    info_message("The operation is not support!");
+                    break;
             }
-                
-            case 'q':
-                return 0;
-                break;
-                
-            default:
-                info_message("The operation is not support!");
-                break;
+        } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
         }
     }
 }
